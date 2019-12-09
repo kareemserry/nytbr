a6aae6ebdb6ce36417ab707f5a0449cbf3e30246
TheKareemOne <thekareem13@gmail.com>
2019-12-09 16:11:36 +0200

    Added Books Page

---
+++ frontend/src/components/books/Books.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { GridList, GridListTile, Container } from '@material-ui/core';
+import Regular from './Regular';
+
+export default class Books extends Component {
+    constructor() {
+        super();
+        this.state = {
+            fiction: [],
+            nonFiction: []
+        };
+    }
+    getBooks = async () => {
+        const res = await fetch(`http://localhost:5000/books/`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+        const json = await res.json();
+        if (json.error) {
+            console.log(json.error);
+        } else {
+            this.setState({
+                fiction: json.fiction,
+                nonFiction: json.nonFiction
+            })
+            console.log(json);
+        }
+    }
+    componentDidMount() {
+        this.getBooks();
+    }
+    render() {
+        return (
+            <div>
+                {this.state.fiction.length > 0 ? <h3 style={{ textAlign: 'center' }}>FICTION</h3> : <div />}
+                <GridList className="mt-5 mb-5" style={{ justifyContent: 'center' }}>
+                    {this.state.fiction.map((book, index) => (
+                        <Regular key={index} book={book} />
+                    ))}
+                </GridList>
+                {this.state.nonFiction.length > 0 ? <h3 style={{ textAlign: 'center' }}>NON-FICTION</h3> : <div />}
+                <GridList className="mt-5 mb-5" style={{ justifyContent: 'center' }}>
+                    {this.state.nonFiction.map((book, index) => (
+                        <Regular key={index} book={book} />
+                    ))}
+                </GridList>
+            </div>
+
+        )
+    }
+}
